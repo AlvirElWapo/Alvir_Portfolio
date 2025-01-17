10593f956ff9a766741371864368e93b11be3311
AlvirElWapo <AlvirElWapo>
2025-01-16 19:37:39 -0600

    Add: my info, pending info to add to the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+// import starlight from '@astrojs/starlight';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()]
+    // starlight()]
 });
