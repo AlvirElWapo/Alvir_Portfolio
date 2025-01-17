54a102276f26b1713d4da4d12e7c7e9a6d274a39
AlvirElWapo <AlvirElWapo>
2025-01-16 23:04:11 -0600

    ADD: Completed icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import icon from 'astro-icon';
+
 // import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), 
+    icon({
+      iconDir: "src/icons",
+    })
+  ]
     // starlight()]
 });
